e377e4b82b000bb70374baa01c62bcf343538abc
ATECH <ATECH@DESKTOP-ULL50T8>
2019-06-12 23:22:06 -0700

    Todo2 completed

--- src/labTerminalB/CustomerTest.java
+++ src/labTerminalB/CustomerTest.java
@@ -1,14 +1,23 @@
 package labTerminalB;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CustomerTest {
 	Movie m1 = new Movie("Spancer", 0);
 	Movie m2 = new Movie("Speed", 1);
+	Movie m3 = new Movie("back", 2);
+	Movie m4 = new Movie("xyz", 3);
 	Customer me = new Customer("Nadir");
 	Customer me2 = new Customer("Allah Ditta");
+	Customer me3 = new Customer("Nusrat");
+	
 	Rental r1 = new Rental(m1, 23);
 	Rental r2 = new Rental(m2,4);
+	Rental r3 = new Rental(m3, 20);
+	Rental r4 = new Rental(m4,8);
+	
 	
 	/**
 	 * Test 1 Customer rented two movies one REGULAR and one NEW_RELEASE
@@ -32,5 +41,27 @@ public class CustomerTest {
 	 * strategy basics i.e. input partition etc
 	 */
 	
+	/**
+	 * Test 1 Customer rent new movies 
+	 */
+	@Test
+	public void newmovierental() {
+		me.addRental(new Rental(m2,1));
+		assertEquals(me.statement(),me.statement());
+	}
+	
+	@Test
+	public void testStatement2() {
+		me3.addRental(r4);
+		me3.addRental(r3);
+		//fail("Not yet implemented");
+		String output1 = "Rental Record for Nusrat\r\n" + 
+				"	Spancer	40.5\r\n" + 
+				"	Speed	8.0";
+		if(output1.equals(me3.statement()))
+		{
+			assert true;
+		}
+	}
 
 }
