aed7f41241e99eb26882dda9339e1f24e2ef6e50
ATECH <ATECH@DESKTOP-ULL50T8>
2019-06-12 23:48:53 -0700

    commit 4

--- src/labTerminalB/Driver.java
+++ src/labTerminalB/Driver.java
@@ -1,5 +1,8 @@
 package labTerminalB;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Driver {
 	/*
 	 * TODO 1	10 Marks
@@ -13,22 +16,27 @@ public class Driver {
 		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
 		String regex = "<[^>]*>";
 		String output = input.replaceAll(regex, "");
+		
+		Pattern p=Pattern.compile(regex);
+		Matcher m = p.matcher(input);
 		System.out.println(output);
 		
 		
-		Movie m1 = new Movie("Spancer", 0);
-		Movie m2 = new Movie("Speed", 1);
-		Movie m3 = new Movie("Blood Diamond",2);
-		Customer me = new Customer("Nadir");
-		Customer me2 = new Customer("Allah Ditta");
-		Rental r1 = new Rental(m1, 23);
-		Rental r2 = new Rental(m2,4);
-		Rental r3 = new Rental(m3,3);
+		Movie m1=new Movie("Men in Black",0);
+		Movie m2=new Movie("Avengers End Game",1);
+	Movie m3=new Movie("Friendships",2);
+		
+		Customer me=new Customer("Sharjeel");
+		//Customer me2=new Customer("Allah Ditta");
+		Rental r1=new Rental(m1,4);
+		Rental r2=new Rental(m2,5);
+		Rental r3=new Rental(m3,4);
 		me.addRental(r1);
 		me.addRental(r2);
-		me2.addRental(r3);
+		me.addRental(r3);
 		System.out.println(me.statement());
-		System.out.println(me2.statement());
+		//System.out.println(me2.statement());
 
 	}
 }
+
